a7864188a64529cd1e1d979e66362f97af6f814d
אורי בן-ארצי <oribena@post.bgu.ac.il>
2020-06-11 16:29:45 +0300

    11.6

--- src/main/java/View/MyViewController.java
+++ src/main/java/View/MyViewController.java
@@ -1,9 +1,13 @@
 package View;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 
 public class MyViewController implements IView{
     public void pushButton(ActionEvent actionEvent) {
         System.out.println("test");
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setContentText("hiiii");
+        alert.show();
     }
 }
