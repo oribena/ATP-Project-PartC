f0bd58ee01cc745cc20cc72ec0a73ceefce426ec
אורי בן-ארצי <oribena@post.bgu.ac.il>
2020-06-11 15:19:42 +0300

    11.6

--- src/main/java/View/MyViewController.java
+++ src/main/java/View/MyViewController.java
@@ -1,4 +1,9 @@
 package View;
 
+import javafx.event.ActionEvent;
+
 public class MyViewController implements IView{
+    public void pushButton(ActionEvent actionEvent) {
+        System.out.println("test");
+    }
 }
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,16 +1,21 @@
 package sample;
 
+import View.MyViewController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.imageio.IIOParam;
+import java.util.Observable;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../View/MyView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Parent root = fxmlLoader.load(getClass().getResource("../View/MyView.fxml").openStream());
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
